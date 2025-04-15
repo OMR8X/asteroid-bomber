fc7aa7600d17b28e6f02de97d764bb799b7f8390
Laeth234al <laeth2004alhalabe@gmail.com>
2025-04-15 14:29:59 +0300

    create Timer & Animation & bloc Builder

--- lib/screens/asteriods_view.dart
+++ lib/screens/asteriods_view.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:asteroid_bomber/blocs/asteriods_bloc/asteriods_bloc.dart';
 import 'package:asteroid_bomber/resources/colors_resources.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AsteroidsView extends StatefulWidget {
   const AsteroidsView({super.key});
@@ -9,17 +14,61 @@ class AsteroidsView extends StatefulWidget {
 }
 
 class _AsteroidsViewState extends State<AsteroidsView> {
+  late AsteriodsBloc asteriodsBloc;
+  late Timer asteroidTimer;
+
+  @override
+  void initState() {
+    super.initState();
+    asteriodsBloc = AsteriodsBloc();
+    asteriodsBloc.add(AddAsteriodEvent());
+    asteroidTimer = Timer.periodic(Duration(milliseconds: 300), (timer) {
+      asteriodsBloc.add(UpdateAsteriodEvent());
+
+      // 10%
+      if (Random().nextInt(100) < 10) {
+        asteriodsBloc.add(AddAsteriodEvent());
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    asteroidTimer.cancel();
+    asteriodsBloc.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GestureDetector(
-        onPanUpdate: (details) {},
-        onPanEnd: (details) {},
-        onPanDown: (details) {},
+      body: BlocProvider.value(
+        value: asteriodsBloc,
         child: Container(
           color: ColorsResources.surface,
-          child: const Stack(
-            children: [],
+          child: Stack(
+            children: [
+              BlocBuilder<AsteriodsBloc, AsteriodsState>(
+                builder: (context, state) {
+                  return Stack(
+                    children: state.asteroids.map((asteroid) {
+                      return AnimatedPositioned(
+                        key: ValueKey(asteroid.id),
+                        duration: Duration(milliseconds: 300),
+                        curve: Curves.linear,
+                        left: asteroid.line * (MediaQuery.sizeOf(context).width / 7),
+                        top: asteroid.position,
+                        child: SizedBox(
+                          width: 50,
+                          height: 50,
+                          child: Image.asset(asteroid.imagePath),
+                        ),
+                      );
+                    }).toList(),
+                  );
+                },
+              ),
+            ],
           ),
         ),
       ),
