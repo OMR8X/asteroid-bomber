8cd7560311285ec7f63f3b82315271edc496d2e0
Laeth234al <laeth2004alhalabe@gmail.com>
2025-04-15 14:28:06 +0300

    add image path of asteroids to colors resources

--- lib/resources/images_resources.dart
+++ lib/resources/images_resources.dart
@@ -1,4 +1,5 @@
 class ImagesResources {
   static const String basePath = 'assets/images/';
   static const String rocketImagePath = 'assets/images/rocket.png';
+  static const String asteroidImagePath = 'assets/images/asteroid_';
 }
