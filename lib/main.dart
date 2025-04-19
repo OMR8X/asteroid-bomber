aa541daaacfe4ba335ad9c71aa22efd20bc047e1
A B O U D <aboudkalaf10@gmail.com>
2025-04-19 14:22:37 +0300

    repleace bullet painter with image

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,8 @@ startUpdateFramesTimer() {
     sl<RocketBloc>().add(BulletsUpdatedEvent());
     //
     if (timer.tick % 1 == 0) {
-      final rocketTipX = sl<RocketBloc>().state.rocketPosition.dx + (LayoutConstants.rocketSize.width / 2);
+      final rocketTipX = sl<RocketBloc>().state.rocketPosition.dx +
+          (LayoutConstants.rocketSize.width / 2);
       final rocketTipY = sl<RocketBloc>().state.rocketPosition.dy;
       sl<RocketBloc>().add(BulletFiredEvent(Offset(rocketTipX, rocketTipY)));
     }
