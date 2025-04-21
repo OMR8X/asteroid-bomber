18aad12cffa4780e6162f2f3eb16776fc9807ff4
Laeth234al <laeth2004alhalabe@gmail.com>
2025-04-21 19:01:59 +0300

    add animation bullet & sound of bullet

--- lib/blocs/background_sounds_bloc/background_sounds_bloc.dart
+++ lib/blocs/background_sounds_bloc/background_sounds_bloc.dart
@@ -6,14 +6,23 @@ part 'background_sounds_event.dart';
 part 'background_sounds_state.dart';
 
 class BackgroundSoundsBloc extends Bloc<BackgroundSoundsEvent, BackgroundSoundsState> {
-  final player = AudioPlayer();
+  final AudioPlayer backgroundPlayer = AudioPlayer(playerId: "bg_player");
+  final AudioPlayer effectPlayer = AudioPlayer(playerId: "effect_player");
   BackgroundSoundsBloc() : super(BackgroundSoundsInitial()) {
     on<BackgroundSoundsStartEvent>((event, emit) async {
-      await player.setVolume(0.5);
-      await player.play(AssetSource(SoundsRecourses.sound1));
+      await backgroundPlayer.setPlayerMode(PlayerMode.mediaPlayer);
+      await backgroundPlayer.setReleaseMode(ReleaseMode.loop);
+      await backgroundPlayer.setVolume(0.5);
+      await backgroundPlayer.play(AssetSource(SoundsRecourses.sound1));
     });
     on<BackgroundSoundsStopEvent>((event, emit) async {
-      await player.stop();
+      await backgroundPlayer.stop();
+    });
+    on<BackgroundSoundsExplosionEvent>((event, emit) async {
+      await effectPlayer.setPlayerMode(PlayerMode.lowLatency);
+      //await explosionPlayer.stop();
+      await effectPlayer.setVolume(0.8);
+      await effectPlayer.play(AssetSource(SoundsRecourses.beoSound));
     });
   }
 }
--- lib/models/bullet_model.dart
+++ lib/models/bullet_model.dart
@@ -2,12 +2,24 @@ import 'dart:ui';
 
 class BulletModel {
   final Offset position;
+  final bool isExploding;
+  final DateTime? explosionStartTime;
 
-  BulletModel({required this.position});
+  BulletModel({
+    required this.position,
+    required this.isExploding,
+    this.explosionStartTime,
+  });
 
-  BulletModel copyWith({Offset? position}) {
+  BulletModel copyWith({
+    Offset? position,
+    bool? isExploding,
+    DateTime? explosionStartTime,
+  }) {
     return BulletModel(
       position: position ?? this.position,
+      isExploding: isExploding ?? this.isExploding,
+      explosionStartTime: explosionStartTime ?? this.explosionStartTime,
     );
   }
 }
